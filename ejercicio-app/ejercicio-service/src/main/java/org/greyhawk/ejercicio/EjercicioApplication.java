bf04242f5aa21e843df2269ab19392026fd545ff
Eloy Fernandez Gallut <eloy.fernandezgallut@amaris.com>
2022-03-22 13:26:58 +0100

    Modulo Service

---
+++ ejercicio-app/ejercicio-service/src/main/java/org/greyhawk/ejercicio/EjercicioApplication.java
@@ -0,0 +1,15 @@
+package org.greyhawk.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
+@SpringBootApplication
+public class EjercicioApplication {
+
+  public static void main(final String[] args) {
+    SpringApplication.run(EjercicioApplication.class, args);
+
+  }
+
+}
